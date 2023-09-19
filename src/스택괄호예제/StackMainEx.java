42619ee3a5a3de1571e0b2b092067b1528020841
ydj1215 <ydjdj2@naver.com>
2023-09-19 15:55:13 +0900

    20230919

--- src/스택괄호예제/StackMainEx.java
+++ src/스택괄호예제/StackMainEx.java
@@ -8,16 +8,24 @@ public class StackMainEx {
         Stack<Character> st = new Stack<>();
         Scanner sc = new Scanner(System.in);
         System.out.println("수식 입력: ");
-        String n = sc.next();
+        String string = sc.next();
 
-        for(int i=0; i<n.length(); i++){
-            char ch = n.charAt(i);
+        for(int i=0; i<string.length(); i++){
+            char ch = string.charAt(i);
             if(ch == '(') {
                 st.push(ch);
             } else if(ch==')'){
-
+                if(!st.isEmpty())st.pop();
+            }
+            else{
+                System.out.println("괄호가 일치하지 않습니다.");
             }
         }
-
+        if(st.isEmpty()){
+            System.out.println("괄호가 일치합니다.");
+        }
+        else {
+            System.out.println("괄호가 일치하지 않습니다.");
+        }
     }
 }
